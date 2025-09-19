e9e00dc552e1ed42777ca482e4d2ab1dd540a9ff
cgsizem3_ncstate <cgsizme3@ncsu.edu>
2025-09-19 19:44:02 -0400

    File that retrieves all undergraduates ncsu students

---
+++ directory.py
@@ -0,0 +1,50 @@
+from ldap3 import Server, Connection, ALL, SUBTREE
+import getpass
+
+password = getpass.getpass("Password: ")
+
+# AD host with SSL on port 636
+server = Server("ldaps.wolftech.ad.ncsu.edu", port=636, use_ssl=True, get_info=ALL)
+
+# Use UPN (unityid@ncsu.edu) for bind
+user = "cgsizem3@ncsu.edu"
+
+conn = Connection(server, user=user, password=password, auto_bind=True)
+
+print("✅ Successfully bound to LDAP!")
+
+# Perform the search
+# conn.search(search_base, search_filter, attributes=['sAMAccountName', 'givenName', 'sn', 'userPrincipalName'])
+
+# conn.search(
+#     search_base="DC=wolftech,DC=ad,DC=ncsu,DC=edu",
+#     search_filter="(&(givenName=Jonathan)(sn=P*))",
+#     search_scope=SUBTREE,
+#     attributes=["cn", "mail", "sAMAccountName", "memberOf"]
+# )
+search_base='DC=wolftech,DC=ad,DC=ncsu,DC=edu',
+search_filter="(&(objectClass=user)(memberOf=CN=WT-NCSU-Students-Undergraduate,OU=Managed Groups,OU=NCSU,DC=wolftech,DC=ad,DC=ncsu,DC=edu))"
+search_scope=SUBTREE
+attributes=['sAMAccountName', 'givenName', 'sn', 'mail', 'ncsuCampusID']
+
+all_entries = []
+
+page_size = 5000
+cookie = None
+while True:
+    conn.search(
+        search_base,
+        search_filter,
+        search_scope=SUBTREE,
+        attributes=attributes,
+        paged_size=page_size,
+        paged_cookie=cookie
+    )
+    all_entries.extend(conn.entries)
+    cookie = conn.result['controls']['1.2.840.113556.1.4.319']['value']['cookie']
+    if not cookie:
+        break
+
+print(f"Total undergrads found: {len(all_entries)}")
+
+
