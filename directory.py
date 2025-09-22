93a0aa63cb7e49691d8589e1c6dd6065b00823b9
cgsizem3_ncstate <cgsizme3@ncsu.edu>
2025-09-21 23:08:09 -0400

    Added file that searches for one student based on name, added file that will become the sample builder

--- directory.py
+++ directory.py
@@ -1,13 +1,15 @@
 from ldap3 import Server, Connection, ALL, SUBTREE
-import getpass
+from dotenv import load_dotenv
+import os
 
-password = getpass.getpass("Password: ")
+load_dotenv()
+password = os.getenv("PASSWORD")
+user = os.getenv("USER")
 
 # AD host with SSL on port 636
 server = Server("ldaps.wolftech.ad.ncsu.edu", port=636, use_ssl=True, get_info=ALL)
 
 # Use UPN (unityid@ncsu.edu) for bind
-user = "cgsizem3@ncsu.edu"
 
 conn = Connection(server, user=user, password=password, auto_bind=True)
 
@@ -16,7 +18,7 @@ print("✅ Successfully bound to LDAP!")
 search_base='DC=wolftech,DC=ad,DC=ncsu,DC=edu',
 search_filter="(&(objectClass=user)(memberOf=CN=WT-NCSU-Students-Undergraduate,OU=Managed Groups,OU=NCSU,DC=wolftech,DC=ad,DC=ncsu,DC=edu))"
 search_scope=SUBTREE
-attributes=['sAMAccountName', 'givenName', 'sn', 'mail', 'ncsuCampusID']
+attributes=['sAMAccountName', 'givenName', 'sn', 'mail', 'ncsuCampusID', 'memberOf']
 
 all_entries = []
 
@@ -35,6 +37,8 @@ while True:
     cookie = conn.result['controls']['1.2.840.113556.1.4.319']['value']['cookie']
     if not cookie:
         break
+
+print(all_entries[0])
 
 print(f"Total undergrads found: {len(all_entries)}")
 
---
+++ one_student.py
@@ -0,0 +1,30 @@
+from ldap3 import Server, Connection, ALL, SUBTREE
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+password = os.getenv("PASSWORD")
+user = os.getenv("USER")
+
+# AD host with SSL on port 636
+server = Server("ldaps.wolftech.ad.ncsu.edu", port=636, use_ssl=True, get_info=ALL)
+
+# Use UPN (unityid@ncsu.edu) for bind
+
+conn = Connection(server, user=user, password=password, auto_bind=True)
+
+print("✅ Successfully bound to LDAP!")
+
+student_first = "Allison"
+student_last = "Waddell"
+
+
+conn.search(
+    search_base="DC=wolftech,DC=ad,DC=ncsu,DC=edu",
+    search_filter=f"(&(givenName={student_first})(sn={student_last}))",
+    search_scope=SUBTREE,
+    attributes=["*"]
+)
+
+for entry in conn.entries:
+    print(entry)
